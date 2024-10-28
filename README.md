e1221b16440b3e3527b07ddf596b06640099d5ee
Rob Rohan <rob@therohans.com>
2024-10-28 14:50:37 +1300

    Playing with displaying displacement maps

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
-# depthmap-viewer-three
+# DepthMap Three.js Viewer Spike
 
-Simple web-based interactive depthmap viewer. Using [threejs](https://threejs.org/) to render a plane with a displacement map.
+(Original copied from here: https://github.com/thygate/depthmap-viewer-three)
 
-`Drag-and-drop` images with combined-rgb-and-depth-horizontally into the window to view them.
-
-LIVE at https://thygate.github.io/depthmap-viewer-three
-
-## Example input image
-
-![example](src/assets/roots2_rgbd.png)
->Image was generated with stable diffusion using [AUTOMATIC1111's Stable Diffusion Web UI](https://github.com/AUTOMATIC1111/stable-diffusion-webui) and [stable-diffusion-webui-depthmap-script](https://github.com/thygate/stable-diffusion-webui-depthmap-script).
+Simple web-based interactive depthmap viewer. Using 
+[threejs](https://threejs.org/) to render a plane with a displacement map.
 
 ## Running
 
@@ -17,13 +11,6 @@ The following installs dev and non-dev dependencies, including `three` for
 rendering, and `serve` which is used to serve the website locally:
 
 ```
-npm clean-install
+npm i
 npm start
-```
-
-To install for production without dev dependencies (namely, without `serve`)
-run the following then host the files anywhere you want:
-
-```
-npm clean-install --production
 ```
--- src/index.js
+++ src/index.js
@@ -2,192 +2,145 @@ import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 
-const MyTexture = new URL("./assets/roots2_rgbd.png", import.meta.url);
+//////////////////////////////////////////////
+
+const RGBTexture = new URL("./assets/AppleBin.jpg", import.meta.url);
+const DepthTexture = new URL("./assets/AppleBin_depth.png", import.meta.url);
+
+const textures = [null, null];
+
+/////////////////////////////////////////////
 
 let mesh;
 let material;
-let image_ar;
+// let image_ar;
 
 const settings = {
 	metalness: 0.0,
-	roughness: 0.14,
-	ambientIntensity: 0.85,
-	displacementScale: 5, 
-	displacementBias: -0.5,
+	roughness: 1.0,
+	ambientIntensity: 1,
+	displacementScale: 1,
+	displacementBias: 0,
 };
 
 // init
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.z = 3;
 
 const scene = new THREE.Scene();
 
-const ambientLight = new THREE.AmbientLight( 0xffffff, 0.5 );
-scene.add( ambientLight );
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight( 0xff0000, 0.5 );
+const pointLight = new THREE.PointLight(0xff0000, 0.5);
 pointLight.position.z = 2500;
-scene.add( pointLight );
+scene.add(pointLight);
 
-
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animation );
-//renderer.xr.enabled = true;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animation);
+// renderer.xr.enabled = true;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1;
 renderer.outputEncoding = THREE.sRGBEncoding;
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
+
 
 // animation
-function animation( time ) {
-
-	//mesh.rotation.x = time / 2000;
-	//mesh.rotation.y = time / 1000;
-
-	renderer.render( scene, camera );
-
+function animation(time) {
+	renderer.render(scene, camera);
 }
 
 function onWindowResize() {
-
 	const aspect = window.innerWidth / window.innerHeight;
 	camera.aspect = aspect;
 	camera.updateProjectionMatrix();
-
-	renderer.setSize( window.innerWidth, window.innerHeight );
-
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
-window.addEventListener( 'resize', onWindowResize );
+window.addEventListener('resize', onWindowResize);
 
 
 // orbit controls
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableZoom = true;
 controls.enableDamping = true;
 
 
-const image = new Image();
-image.onload = function() { 
+{
+	const i1 = new Image();
+	i1.onload = function() {
+		const ctx = document.createElement('canvas').getContext('2d');
+		ctx.canvas.width = i1.width;
+		ctx.canvas.height = i1.height;
+		ctx.drawImage(i1, 0, 0, i1.width, i1.height, 0, 0, i1.width, i1.height);
+		const myrgbmap = new THREE.CanvasTexture(ctx.canvas);
+		textures[0] = myrgbmap;
 
-	if (mesh) {
-		mesh.geometry.dispose();
-		mesh.material.dispose();
-		scene.remove( mesh );
+		window.dispatchEvent(new CustomEvent("texture_loaded", {}));
 	}
-	
-	image_ar = image.width / image.height / 2;
-	
-	const ctx = document.createElement('canvas').getContext('2d');
-	ctx.canvas.width = image.width / 2;
-	ctx.canvas.height = image.height;
-	ctx.drawImage(image, 0, 0, image.width / 2, image.height, 0, 0, image.width / 2, image.height);
-	const myrgbmap = new THREE.CanvasTexture(ctx.canvas);
+	i1.src = RGBTexture;
 
-	const ctx2 = document.createElement('canvas').getContext('2d');
-	ctx2.canvas.width = image.width / 2;
-	ctx2.canvas.height = image.height;
-	ctx2.drawImage(image, image.width / 2, 0, image.width / 2, image.height, 0, 0, image.width / 2, image.height);
-	const mydepthmap = new THREE.CanvasTexture(ctx2.canvas);
+	const i2 = new Image();
+	i2.onload = function() {
+		const ctx2 = document.createElement('canvas').getContext('2d');
+		ctx2.canvas.width = i2.width;
+		ctx2.canvas.height = i2.height;
+		ctx2.drawImage(i2, 0, 0, i2.width, i2.height, 0, 0, i2.width, i2.height);
+		const mydepthmap = new THREE.CanvasTexture(ctx2.canvas);
+		textures[1] = mydepthmap;
 
-	
-	// material
-	material = new THREE.MeshStandardMaterial( {
-
-		color: 0xaaaaaa,
-		roughness: settings.roughness,
-		metalness: settings.metalness,
-
-		map: myrgbmap,
-
-		displacementMap: mydepthmap,
-		displacementScale: settings.displacementScale,
-		displacementBias: settings.displacementBias,
-
-		side: THREE.DoubleSide
-
-	} );
-	
-	// generating geometry and add mesh to scene
-	const geometry = new THREE.PlaneGeometry( 10, 10, 512, 512 );
-	mesh = new THREE.Mesh( geometry, material );
-	mesh.scale.y = 1.0 / image_ar;
-	mesh.scale.multiplyScalar( 0.23 );
-	scene.add( mesh );
-	
-}
-image.src = MyTexture;
-
-
-// setup gui
-const gui = new GUI();
-gui.add( settings, 'metalness' ).min( 0 ).max( 1 ).onChange( function ( value ) {
-	material.metalness = value;
-} );
-gui.add( settings, 'roughness' ).min( 0 ).max( 1 ).onChange( function ( value ) {
-	material.roughness = value;
-} );
-gui.add( settings, 'ambientIntensity' ).min( 0 ).max( 1 ).onChange( function ( value ) {
-	ambientLight.intensity = value;
-} );
-gui.add( settings, 'displacementScale' ).min( 0 ).max( 30.0 ).onChange( function ( value ) {
-	material.displacementScale = value;
-} );
-gui.add( settings, 'displacementBias' ).min( -10 ).max( 10 ).onChange( function ( value ) {
-
-	material.displacementBias = value;
-} );
-
-
-// setup drop zone
-var dropZone = document.getElementById('dropzone');
-
-function showDropZone() {
-	dropZone.style.display = "block";
-}
-function hideDropZone() {
-	dropZone.style.display = "none";
-}
-
-function allowDrag(e) {
-	if (true) {  // Test that the item being dragged is a valid one
-		e.dataTransfer.dropEffect = 'copy';
-		e.preventDefault();
+		window.dispatchEvent(new CustomEvent("texture_loaded", {}));
 	}
+	i2.src = DepthTexture;
 }
 
-function handleDrop(e) {
-	e.preventDefault();
-	hideDropZone();
-	const fileList = event.dataTransfer.files;
-	if (fileList.length > 0) {
-		readImage(fileList[0]);
-	}
-}
-
-function readImage(file) {
-	const reader = new FileReader();
-	reader.addEventListener('load', (event) => {
-		image.src = event.target.result;
+{
+	// setup gui
+	const gui = new GUI();
+	gui.add(settings, 'metalness').min(0).max(1).onChange(function (value) {
+		material.metalness = value;
 	});
-	reader.readAsDataURL(file);
+	gui.add(settings, 'roughness').min(0).max(1).onChange(function (value) {
+		material.roughness = value;
+	});
+	gui.add(settings, 'ambientIntensity').min(0).max(1).onChange(function (value) {
+		ambientLight.intensity = value;
+	});
+	gui.add(settings, 'displacementScale').min(0).max(30.0).onChange(function (value) {
+		material.displacementScale = value;
+	});
+	gui.add(settings, 'displacementBias').min(-10).max(10).onChange(function (value) {
+		material.displacementBias = value;
+	});
 }
 
-window.addEventListener('dragenter', function(e) {
-	showDropZone();
-});
-dropZone.addEventListener('dragenter', allowDrag);
-dropZone.addEventListener('dragover', allowDrag);
-dropZone.addEventListener('dragleave', function(e) {
-	console.log('dragleave');
-	hideDropZone();
-});
-dropZone.addEventListener('drop', handleDrop);
+window.addEventListener('texture_loaded', function (e) {
+	console.log(textures);
+	if(textures[0] != null && textures[1] != null) {
+		if (mesh) {
+			mesh.geometry.dispose();
+			mesh.material.dispose();
+			scene.remove(mesh);
+		}
+		const myrgbmap = textures[0];
+		const mydepthmap = textures[1];
 
+		// material
+		material = new THREE.MeshStandardMaterial({
+			color: 0xaaaaaa,
+			roughness: settings.roughness,
+			metalness: settings.metalness,
+			map: myrgbmap,
+			displacementMap: mydepthmap,
+			displacementScale: settings.displacementScale,
+			displacementBias: settings.displacementBias,
+			side: THREE.DoubleSide
+		});
 
-// listen for messages
-window.addEventListener('message', function(e) {
-  if (e.data?.imagedata) {
-	image.src = e.data.imagedata;
-  }
+		// generating geometry and add mesh to scene
+		const geometry = new THREE.PlaneGeometry(10, 10, 512, 512);
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.scale.multiplyScalar(0.23);
+		scene.add(mesh);
+	}
 });
